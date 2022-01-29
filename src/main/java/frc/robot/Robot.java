842d4a58f59dc5fd6e68f61ee00e909e73e8a397
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2022-01-28 16:58:18 -0800

    no commit message for you! worked on motor control for a fixed position.  added a bunch of stuff to facilitate that

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Axis;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -24,6 +26,7 @@ public class Robot extends TimedRobot
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private double value = (90 / 360.0) * 2048;
 
 
   // defining mechanical aspects such as motors and pneumatics
@@ -47,13 +50,58 @@ public class Robot extends TimedRobot
    */
   @Override
   public void robotInit()
-  {
+  { //* Sorry for mess, will clean later
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption(kCustomAuto, kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    
-  }
+    Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+
+    /* Factory Default all hardware to prevent unexpected behavior */
+		turningMotor0.configFactoryDefault();
+		
+		/* Config the sensor used for Primary PID and sensor direction */
+        turningMotor0.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 
+                                            0,
+				                            30);
+
+		/* Ensure sensor is positive when output is positive */
+		turningMotor0.setSensorPhase(true);
+
+		/**
+		 * Set based on what direction you want forward/positive to be.
+		 * This does not affect sensor phase. 
+		 */ 
+		turningMotor0.setInverted(false);
+		/*
+		 * Talon FX does not need sensor phase set for its integrated sensor
+		 * This is because it will always be correct if the selected feedback device is integrated sensor (default value)
+		 * and the user calls getSelectedSensor* to get the sensor's position/velocity.
+		 * 
+		 * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#sensor-phase
+		 */
+        // turningMotor0.setSensorPhase(true);
+
+		/* Config the peak and nominal outputs, 12V means full */
+		turningMotor0.configNominalOutputForward(0, 30);
+		turningMotor0.configNominalOutputReverse(0, 30);
+		turningMotor0.configPeakOutputForward(1, 30);
+		turningMotor0.configPeakOutputReverse(-1, 30);
+
+		/**
+		 * Config the allowable closed-loop error, Closed-Loop output will be
+		 * neutral within this range. See Table in Section 17.2.1 for native
+		 * units per rotation.
+		 */
+		turningMotor0.configAllowableClosedloopError(0, 0, 30);
+
+		/* Config Position Closed Loop gains in slot0, typically kF stays zero. */
+		turningMotor0.config_kF(0, kGains.kF, 30);
+		turningMotor0.config_kP(0, kGains.kP, 30);
+		turningMotor0.config_kI(0, kGains.kI, 30);
+		turningMotor0.config_kD(0, kGains.kD, 30);
+    }
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
@@ -76,20 +124,9 @@ public class Robot extends TimedRobot
     //uncomment below to turn on intake mechanism
     //intakeMotor.set(1 * 0.85);
 
-
- 
-   
-    if (yButton)
-    {
-      turningMotor0.set(ControlMode.Position, 0);
-      System.out.println("0");
-    } 
-
-    if (bButton)
-    {
-      turningMotor0.set(ControlMode.Position, 90);
-      System.out.println("90");
-    }
+    value = value + util.XYposToRad(leftX, -leftY);
+    turningMotor0.set(TalonFXControlMode.Position, value);
+    System.out.println(value);
 
     //System.out.println(leftX + ", " + -leftY + ", " + util.XYposToRad(leftX,-leftY));
     
