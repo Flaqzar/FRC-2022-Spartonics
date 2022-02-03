bbf2f32bffa5e07267cdce1e6a7420b42a9f1e5b
Flaqzar <51932016+Flaqzar@users.noreply.github.com>
2022-02-02 16:03:04 -0800

    Formatting Fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,13 +1,13 @@
 package frc.robot;
 
-public class Constants {
-    /**
-     * The delay in milliseconds before a report gets sent to DriverStation if an
-     * action fails
-     */
-    public static final int MS_DELAY = 30;
+public class Constants
+{
+	/**
+	 * The delay in milliseconds before a report gets sent to DriverStation if an
+	 * action fails
+	 */
+	public static final int MS_DELAY = 30;
 
-    /** Settings are {kF, kP, kI, kD} */ // F P I D
-    public static final double[] PID_SETTINGS = { 0d, 0.25d, 0d, 1d };
-
+	/** Settings are {kF, kP, kI, kD} */ // F P I D
+	public static final double[] PID_SETTINGS = { 0d, 0.25d, 0d, 1d };
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,130 +15,125 @@ import edu.wpi.first.wpilibj.XboxController;
  * build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
-    private static final String kDefaultAuto = "Default";
-    private static final String kCustomAuto = "My Auto";
-    private String m_autoSelected;
-    private final SendableChooser<String> m_chooser = new SendableChooser<>();
+public class Robot extends TimedRobot
+{
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-    //TODO                                 fix CAN bus id's
-    SwerveModule testswerve = new SwerveModule(0,1);
-    /** Main controller */
-    private final XboxController controller = new XboxController(0);
+	//TODO fix CAN bus id's
+	SwerveModule testswerve = new SwerveModule(0,1);
+	/** Main controller */
+	private final XboxController controller = new XboxController(0);
 
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any
+	 * initialization code.
+	 */
+	@Override
+	public void robotInit()
+	{
+		// ? What does this do?
+		this.m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+		this.m_chooser.addOption(kCustomAuto, kCustomAuto);
+		SmartDashboard.putData("Auto choices", this.m_chooser);
+		
+		this.testswerve.init();
+	}
 
+	/**
+	 * This function is called every robot packet, no matter the mode. Use this for
+	 * items like
+	 * diagnostics that you want ran during disabled, autonomous, teleoperated and
+	 * test.
+	 *
+	 * <p>
+	 * This runs after the mode specific periodic functions, but before LiveWindow
+	 * and
+	 * SmartDashboard integrated updating.
+	 */
+	@Override
+	public void robotPeriodic()
+	{
+		double leftXAxis = this.controller.getRawAxis(0);
+		double leftYAxis = this.controller.getRawAxis(1);
+		double leftJoystickDistance = Math.sqrt(leftXAxis * leftXAxis + leftYAxis * leftYAxis);
 
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any
-     * initialization code.
-     */
-    @Override
-    public void robotInit() {
-        // ? What does this do?
-        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-        m_chooser.addOption(kCustomAuto, kCustomAuto);
-        SmartDashboard.putData("Auto choices", m_chooser);
-        
-        testswerve.init();
-    }
+		// Creates a deadzone of 10%
+		if (leftJoystickDistance < 0.1d)
+		{
+			leftXAxis = 0d;
+			leftYAxis = 0d;
+		}
 
-    /**
-     * This function is called every robot packet, no matter the mode. Use this for
-     * items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and
-     * test.
-     *
-     * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow
-     * and
-     * SmartDashboard integrated updating.
-     */
-    @Override
-    public void robotPeriodic() {
-        double leftXAxis = controller.getRawAxis(0);
-        double leftYAxis = controller.getRawAxis(1);
-        double leftJoystickDistance = Math.sqrt(leftXAxis * leftXAxis + leftYAxis * leftYAxis);
+		double currentRotation = (Math.atan(leftYAxis / leftXAxis) + (leftXAxis < 0d ? Math.PI : 0d) + (Math.PI / 2d)) / (Math.PI * 2d);
+		this.testswerve.setAngle(currentRotation);
+	}
 
-        // Creates a deadzone of 10%
-        if (leftJoystickDistance < 0.1d) {
-            leftXAxis = 0d;
-            leftYAxis = 0d;
-        }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different
+	 * autonomous modes using the dashboard. The sendable chooser code works with
+	 * the Java
+	 * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+	 * chooser code and
+	 * uncomment the getString line to get the auto name from the text box below the
+	 * Gyro
+	 *
+	 * <p>
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure
+	 * below with additional strings. If using the SendableChooser make sure to add
+	 * them to the
+	 * chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit()
+	{
+		this.m_autoSelected = this.m_chooser.getSelected();
+		// this.m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+		System.out.println("Auto selected: " + this.m_autoSelected);
+	}
 
-        // currentRotation += currentRotation - (Math.atan(leftYAxis / leftXAxis) +
-        // (leftXAxis < 0d ? Math.PI : 0d) + (Math.PI / 2d));
-        double currentRotation = (Math.atan(leftYAxis / leftXAxis) + (leftXAxis < 0d ? Math.PI : 0d) + (Math.PI / 2d))
-                / (Math.PI * 2d);
-        //falcon0Rotation = currentRotation * 2048d;
-        testswerve.setAngle(currentRotation);
-    }
+	/** This function is called periodically during autonomous. */
+	@Override
+	public void autonomousPeriodic()
+	{
+		switch (this.m_autoSelected)
+		{
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
 
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different
-     * autonomous modes using the dashboard. The sendable chooser code works with
-     * the Java
-     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-     * chooser code and
-     * uncomment the getString line to get the auto name from the text box below the
-     * Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure
-     * below with additional strings. If using the SendableChooser make sure to add
-     * them to the
-     * chooser code above as well.
-     */
-    @Override
-    public void autonomousInit() {
-        m_autoSelected = m_chooser.getSelected();
-        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-        System.out.println("Auto selected: " + m_autoSelected);
-    }
+	/** This function is called once when teleop is enabled. */
+	@Override
+	public void teleopInit() {}
 
-    /** This function is called periodically during autonomous. */
-    @Override
-    public void autonomousPeriodic() {
-        switch (m_autoSelected) {
-            case kCustomAuto:
-                // Put custom auto code here
-                break;
-            case kDefaultAuto:
-            default:
-                // Put default auto code here
-                break;
-        }
-    }
+	/** This function is called periodically during operator control. */
+	@Override
+	public void teleopPeriodic() {}
 
-    /** This function is called once when teleop is enabled. */
-    @Override
-    public void teleopInit() {
-    }
+	/** This function is called once when the robot is disabled. */
+	@Override
+	public void disabledInit() {}
 
-    /** This function is called periodically during operator control. */
-    @Override
-    public void teleopPeriodic() {
-    }
+	/** This function is called periodically when disabled. */
+	@Override
+	public void disabledPeriodic() {}
 
-    /** This function is called once when the robot is disabled. */
-    @Override
-    public void disabledInit() {
-    }
+	/** This function is called once when test mode is enabled. */
+	@Override
+	public void testInit() {}
 
-    /** This function is called periodically when disabled. */
-    @Override
-    public void disabledPeriodic() {
-    }
-
-    /** This function is called once when test mode is enabled. */
-    @Override
-    public void testInit() {
-    }
-
-    /** This function is called periodically during test mode. */
-    @Override
-    public void testPeriodic() {
-    }
+	/** This function is called periodically during test mode. */
+	@Override
+	public void testPeriodic() {}
 }
