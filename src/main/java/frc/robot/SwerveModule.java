feffded43e62db455c5089a68bbc9454b1ce7ecf
Flaqzar <51932016+Flaqzar@users.noreply.github.com>
2022-02-10 20:38:50 -0800

    Add desmos link

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -5,10 +5,10 @@ import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 /**
- * represents a swerve module. Wraps two falcon500s, one for driving and one for
- * steering
+ * represents a swerve module. Wraps two falcon500s, one for driving and one for steering
  */
-public class SwerveModule {
+public class SwerveModule
+{
 	// Motor objects used in each swerve drive module
 	WPI_TalonFX driveFalcon;
 	WPI_TalonFX steeringFalcon;
@@ -20,7 +20,8 @@ public class SwerveModule {
 	/** Rotation measured in radians. */
 	private double currentRotation;
 
-	public SwerveModule(int driveMotor, int steeringMotor) {
+	public SwerveModule(int driveMotor, int steeringMotor)
+	{
 		// get the motor objects from the CAN bus
 		this.driveFalcon = new WPI_TalonFX(driveMotor);
 		this.steeringFalcon = new WPI_TalonFX(steeringMotor);
@@ -30,7 +31,8 @@ public class SwerveModule {
 	/**
 	 * initialize the motors. Call this before doing stuff with the motor.
 	 */
-	public void init() {
+	public void init()
+	{
 		// Motor settings stuff
 		this.steeringFalcon.configFactoryDefault();
 		this.steeringFalcon.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, PID_ID, Constants.MS_DELAY);
@@ -58,21 +60,22 @@ public class SwerveModule {
 	}
 
 	/**
-	 * 
-	 * @param joystickAngle
+	 * Sets the angle of the motor.
+	 * Calculations by Alex: https://www.desmos.com/calculator/t9mc7gj1bf
+	 * @param angle the angle in radians
 	 */
-	public void setAngle(double joystickAngle)
+	public void setAngle(double angle)
 	{
-		if(!Double.isNaN(joystickAngle))
+		// Make sure that the input angle is a real number.
+		if(!Double.isNaN(angle))
 		{
-			//Clamps the motor's rotation from 0 - 2π
+			// Clamps the motor's rotation from 0 - 2π
 			double motorAngle = this.currentRotation % Constants.TWO_PI;
-			//Adds the angle's difference to the motor's current rotaiton,
-			//adding/subtracting 2π to account for the flip from 2π - 0 on the joystick.
-			this.currentRotation -= motorAngle - joystickAngle + (Math.abs(motorAngle - joystickAngle) > Math.PI ? (motorAngle > joystickAngle ? -Constants.TWO_PI : Constants.TWO_PI) : 0d) + Constants.PI_OVER_TWO;
+			// Adds the two angles' difference to the motor's current rotation
+			this.currentRotation -= motorAngle - angle + (Math.abs(motorAngle - angle) > Math.PI ? (motorAngle > angle ? -Constants.TWO_PI : Constants.TWO_PI) : 0d) + Constants.PI_OVER_TWO;
 		}
 		
-		//Set's the new rotation
+		// Sets the new rotation
  		this.steeringFalcon.set(ControlMode.Position, this.currentRotation / Constants.TWO_PI * 26214.4d);
 	}
 
@@ -80,7 +83,7 @@ public class SwerveModule {
 	 * Converts a jotstick's x and y coordinates into a radian angle from 0 - 2π.
 	 * @param x the joystick's x position
 	 * @param y the joystick's y position
-	 * @return The joystick's rotation as an angle from 0 - 2π.
+	 * @return The joystick's rotation in radians.
 	 */
 	public static double convertJoystickToAngle(double x, double y)
 	{
